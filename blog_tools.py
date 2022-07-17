6bda3d9c090dac2e001325d0938e4998ca1fcb4c
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-07-17 21:35:26 +1000

    Working!

--- blog_tools.py
+++ blog_tools.py
@@ -1,8 +1,8 @@
 import sublime
 import sublime_plugin
 from typing import Callable, Optional
-# from OpenSplit.target_file import TargetFile
 from datetime import date
+from xml.dom.minidom import parseString
 
 class BlogToolsCommand(sublime_plugin.WindowCommand):
   def run(self):
@@ -35,20 +35,43 @@ class BlogToolsCommand(sublime_plugin.WindowCommand):
       print(f"Blog title: {blog_title}")
       print(f"file_name: {file_name_with_date}")
 
-      if folder := self.get_folder_name():
-        file_name: str = f"{folder}/posts/{file_name_with_date}"
+      snippet_file_path_op = self.get_snippet_file_name("meta_md")
+
+      if snippet_file_path_op is not None:
+        snippet_file_path = snippet_file_path_op
+        print(f"snippet file path: {snippet_file_path}")
         try:
-          with open(file_name, 'x') as f: #create only if it does not exist
-            f.write("")
+
+          with open(snippet_file_path, 'r') as snippet_file:
+            xml_content = snippet_file.read()
+
+          template_content_op = self.get_snippet_content(xml_content)
+          if template_content_op is not None:
+            template_content = template_content_op.lstrip().rstrip()
+            print(f"template_content: {template_content}")
+            expanded_snippet = sublime.expand_variables(template_content, {"1": blog_title, "2": blog_title.lower(), "3": "", "4": "inspiration awaits..."})
+            print(f"expanded snippet: {expanded_snippet}")
+            if folder := self.get_folder_name():
+              file_name: str = f"{folder}/posts/{file_name_with_date}"
+              try:
+                with open(file_name, 'x') as f: #create only if it does not exist
+                  f.write(expanded_snippet)
+              except Exception as e:
+                sublime.message_dialog(f"could not create {file_name}: {e}")
+
+              lines = len(template_content.split('\n'))
+              print(f"lines: {lines}")
+              view = self.window.open_file(f"{file_name}:{lines}:0", sublime.ENCODED_POSITION)
+              # self.async_check_for_view(view, lambda v: self.insert_snippet(blog_title, v))
+            else:
+              sublime.message_dialog("Could find current directory")
+          else:
+            sublime.message_dialog("Template content is None")
+
         except Exception as e:
-          sublime.message_dialog(f"could not create {file_name}: {e}")
-
-        view = self.window.open_file(file_name)
-        self.async_check_for_view(view, lambda v: self.insert_snippet(blog_title, v))
-
-
+          sublime.message_dialog(f"Could find load snippet file: {e}")
       else:
-        sublime.message_dialog("Could find current directory")
+        print(f"snippet file path is None")
     else:
       sublime.message_dialog("Could not find Window")
 
@@ -63,6 +86,14 @@ class BlogToolsCommand(sublime_plugin.WindowCommand):
     else:
       callback(view)
 
+  def get_snippet_file_name(self, snippet_name: str) -> Optional[str]:
+    window = self.window
+
+    if window:
+      return sublime.expand_variables(f"$packages/User/snippets/{snippet_name}.sublime-snippet", window.extract_variables())
+    else:
+      sublime.message_dialog("Could not find Window")
+
   def get_folder_name(self) -> Optional[str]:
     window = self.window
     if window:
@@ -74,30 +105,14 @@ class BlogToolsCommand(sublime_plugin.WindowCommand):
     else:
       return None
 
-  def insert_snippet(self, title: str, view: sublime.View) -> None:
-    window = self.window
-
-    description = title.lower()
-    if window and view:
-      # There doesn't seem to be a nice way to load snippet content with variables pre-supplied
-      # hardcoding this here for now.
-      snippet_file = \
-f"""---
-title: {title}
-author: sanjiv sahayam
-description: {description}
-tags:
-comments: true
----
-
-$1
-"""
-      print(f"view: {view}")
-
-      if view.is_loading():
-        print("view is still loading, giving up")
-      else:
-        print("view had loaded")
-        view.run_command("insert_snippet", { "contents":  snippet_file})
-    else:
-      sublime.message_dialog("Could not find Window or View")
+  def get_snippet_content(self, snippet_template_content: str) -> Optional[str]:
+    try:
+        result: str = \
+          parseString(snippet_template_content) \
+          .getElementsByTagName('content')[0] \
+          .firstChild \
+          .wholeText
+        return result
+    except Exception as e:
+      print(f"Could not parse XML: {snippet_template_content} because of: {e}")
+      return None
