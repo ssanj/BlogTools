5e26a031de49680745b6a848a97f0c8fe4ac36bd
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-07-17 17:38:41 +1000

    WIP

--- blog_tools.py
+++ blog_tools.py
@@ -1,7 +1,8 @@
 import sublime
 import sublime_plugin
-# from typing import Optional
+from typing import Optional
 # from OpenSplit.target_file import TargetFile
+from datetime import date
 
 class BlogToolsCommand(sublime_plugin.WindowCommand):
   def run(self):
@@ -41,7 +42,37 @@ class BlogToolsCommand(sublime_plugin.WindowCommand):
       print("No active window found")
 
   def post_name_given(self, input: str) -> None:
-    print(f"you said: {input}")
+    today = date.today()
+    formatted_date = today.strftime("%Y-%m-%d")
+
+    file_name_title: str = input.lower().replace(" ", "-")
+    file_name_with_date: str = f"{formatted_date}-{file_name_title}.md"
+    blog_title: str = input.title()
+
+    print(f"Blog title: {blog_title}")
+    print(f"file_name: {file_name_with_date}")
+
+    if folder := self.get_folder_name():
+      file_name: str = f"{folder}/posts/{file_name_with_date}"
+      try:
+        with open(file_name, 'x') as f: #create only if it does not exist
+          f.write(blog_title)
+
+      except Exception as e:
+        sublime.message_dialog(f"could not create {file_name}: {e}")
+    else:
+      sublime.message_dialog("Could find current directory")
+
+  def get_folder_name(self) -> Optional[str]:
+    window = self.window
+    if window:
+      variables = window.extract_variables()
+      if variables:
+        return variables.get('folder') # could be None
+      else:
+        return None
+    else:
+      return None
 
   # def get_target_file(self, first_match: sublime.SymbolLocation) -> TargetFile:
   #   file_name = first_match.path
